04b566f994615ce244928443510fb147b14b46e0
Victorine Penot <vpenot001@gmail.com>
2023-02-22 09:08:27 +0100

    Lecture image

---
+++ affimg.m
@@ -0,0 +1,24 @@
+clear,
+close all,
+clc,
+
+adr = './database/test1/';
+fld = dir(adr);
+nb_elt = length(fld);
+% Data matrix containing the training images in its columns 
+data_trn = []; 
+% Vector containing the class of each training image
+lb_trn = []; 
+for i=1:nb_elt
+    if fld(i).isdir == false
+        lb_trn = [lb_trn ; str2num(fld(i).name(6:7))];
+        img = double(imread([adr fld(i).name]));
+        imgv = img( :) ;
+        vect(1,i,:)=imgv;
+        imagesc(img) ;
+        colormap(gray) ;
+        drawnow;
+        pause(0.1);
+        data_trn = [data_trn img(:)];
+    end
+end
---
+++ main.m
@@ -0,0 +1,9 @@
+clear,
+close all,
+clc,
+
+%% Chargement/affichage des images
+
+filenames = unzip("database.zip");
+
+vect=affimg();
